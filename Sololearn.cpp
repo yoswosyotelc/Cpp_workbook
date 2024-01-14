c3a0abc15dc39ab76a6e760b3792b22e7b3c8b87
yoswos <151448407+yoswosyotelc@users.noreply.github.com>
2024-01-14 19:02:32 +0300

    Sololearn.cpp

--- Sololearn.cpp
+++ Sololearn.cpp
@@ -1,31 +1,103 @@
-// @#$!@#$@!#$Progress### << ///Dec_2023..??>> @ @#$@%#!@%!Saving??@!#$@!#$@!#$!@#$@!#$
-// !!!!! don't stay at one part for more than 1 weekds...!!!!
-// !!!!! proceed as you go through by commenting .. for furhter refirinement..
-// //..//...//..//...//..//..//..//...//
-#include <iostream>
-using namespace std;
+//@#$!@#$@!#$Progress### << ///Jan_2023..??>> @ @#$@%#!@%!Saving??@!#$@!#$@!#$!@#$@!#$
+//!!!!! don't stay at one part for more than 1 weekds...!!!!
+//!!!!! proceed as you go through by commenting .. for furhter refirinement..
+//..//...//..//...//..//..//..//...//
+//..//..Code explanation.//..use...//.ChatGPT.//..//..//...//
+//..//...//..//...//..//..//..//...//
 
 
 
-
-
+/*___________Codecoach Challenges__________________ */
 
 /*
-#include <iostream>
-using namespace std;
-
-int main()
-{
-	cout << "Hello world cpp" << endl;
-	return 0;
-}
 
 
-int main()
-{	// declaring and input output okay.. using cpp 
-	int name;
-	cin>> name;
-	cout<<name <<endl;
-}
 
-//what is list for cpp .. why?? it has created a .exe.. file.. there ?
+*/
+
+
+/*Intro_C_______________ Basic Concepts _________________*/
+
+/*
+
+//_________________ Numbers
+//______ Output
+//_________________ Numbers
+//______ Output
+//_________________ Numbers
+//______ Output
+//_________________ Numbers
+//______ Output
+//_________________ Numbers
+//______ Output
+
+
+
+
+
+*/
+
+
+
+/*Intro_C_______________ Conditinals and Loops _________________*/
+
+/*
+
+//_________________ Numbers
+//______ Output
+//_________________ Numbers
+//______ Output
+//_________________ Numbers
+//______ Output
+//_________________ Numbers
+//______ Output
+//_________________ Numbers
+//______ Output
+
+
+
+
+*/
+
+
+/*Intro_C_______________ Arrays & Strings _________________*/
+
+/*
+
+//_________________ Numbers
+//______ Output
+//_________________ Numbers
+//______ Output
+//_________________ Numbers
+//______ Output
+//_________________ Numbers
+//______ Output
+//_________________ Numbers
+//______ Output
+
+
+
+
+*/
+
+
+/*Intro_C_______________ Functions & Pointers _________________*/
+
+/*
+
+//_________________ Numbers
+//______ Output
+//_________________ Numbers
+//______ Output
+//_________________ Numbers
+//______ Output
+//_________________ Numbers
+//______ Output
+//_________________ Numbers
+//______ Output
+
+
+
+
+*/
+
